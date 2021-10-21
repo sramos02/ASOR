902ac8bc04cc83fdd2fcf526bf7b0c66817a0d0f
Sergio <31887668+sramos02@users.noreply.github.com>
2021-10-21 18:38:03 +0200

    Add files via upload

--- ej1.cpp
+++ ej1.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 using namespace std;
 
 int main() {
-   if(setuid(0) < 0){
-	perror("setuid error");
-   }
-  else  cout << "Se ha completado la operación\n";
+   int uid;
+   cout << "Introduzca el nuevo uid: ";
+   cin >> uid;
+
+   errno = setuid(uid);
+  
+   if(errno == 0) cout << "Se ha completado la operación\n";
+   else perror("setuid error");
    return 1;
 }
