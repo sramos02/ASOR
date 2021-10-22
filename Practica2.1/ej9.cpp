ff02ee3eaf03bae6f14d4bc5f4caf26ed6be1a1e
Sergio <31887668+sramos02@users.noreply.github.com>
2021-10-22 09:41:25 +0200

    Update ej9.cpp

--- Practica2.1/ej9.cpp
+++ Practica2.1/ej9.cpp
@@ -5,5 +5,7 @@ using namespace std;
 
 int main(){
     cout << "Tu uid es: " << getuid() << "\n";
+    // si el fichero de programa tiene los bits setuid o setgid activos, 
+    // el EUID o el EGID del proceso creado se cambian al usuario o grupo del fichero
     return 0;
 }
