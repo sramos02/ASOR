5f8bf673b9c0e54742b556768f1801ee385f9c8e
Sergio <31887668+sramos02@users.noreply.github.com>
2021-11-10 14:34:12 +0100

    Add files via upload

---
+++ Practica2.2/ej12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]){
+	if(argc != 2){
+		printf("Numero incorrectos de parametros [RUTA]");
+		return -1;
+	}
+
+	int original_stdout = dup(1);
+	int fd = open(argv[1], O_WRONLY |  O_CREAT);
+	dup2(fd,1);
+	close(fd);
+
+	printf("Hello world\n");
+	return 0;
+}
