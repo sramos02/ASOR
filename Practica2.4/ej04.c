f7ef7ceea6e08ea114db228aee52d7cda1adde96
sramos02 <sramos02@ucm.es>
2021-12-02 17:55:04 +0100

    Added file via upload

--- Practica2.4/ej04.c
+++ Practica2.4/ej04.c
@@ -8,12 +8,12 @@
 
 int main(int argc, char* argv[]){
 
-	char *name = "03pipe";
-
+	char* name = pipe;
 	if(argc != 2){
 		perror("Arguments: File");
 		return -1;
 	}
+
 	int fd = open(name, O_RDONLY);
 	if(fd == -1){
 		perror("Open file");
