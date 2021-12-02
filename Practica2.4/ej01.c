5d83e1ae98c27fb815d087deb688c81cf424ee65
Sergio <31887668+sramos02@users.noreply.github.com>
2021-12-02 11:41:26 +0100

    Add files via upload

--- Practica2.4/ej01.c
+++ Practica2.4/ej01.c
@@ -3,35 +3,37 @@
 
 int main(int argc, char* argv[]){
 
-	for(int i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-
 	if(argc != 5){
 		perror("Input error: Command1 Command2");
 		return -1;
 	}
 
 	int fd[2];
-        int pipe = pipe(fd);
-
-	pid_t act;
-	act = fork();
-
-	if(act == -1){
-		perror("Fork");
+	if(pipe(fd) == -1){
+		perror("pipe");
 		return -1;
 	}
-	else if (act == 0){ //HIJO
+
+
+	pid_t pid = fork();
+	switch(pid){
+	case -1:
+		perror("Fork");
+		return -1;
+
+	case 0: //HIJO
 		close(fd[1]);
 		dup2(fd[0],0); //Se cambia el descriptor a la entrada
 		close(fd[0]);
-		execlp(argv[3], argv[3], argv[4]); //Ejecuta el comando
-	}
-	else{ //PADRE
+		execlp(argv[3], argv[3], argv[4], NULL); //Ejecuta el comando
+		break;
+
+	default: //PADRE
 		close(fd[0]);
 		dup2(fd[1],1);
 		close(fd[1]);
-		execlp(argv[1], argv[1], argv[2]);
+		execlp(argv[1], argv[1], argv[2], NULL);
+		break;
 	}
 	return 0;
 }
