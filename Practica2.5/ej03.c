fbd252fe147d05348d55584aa966d563be53285c
Sergio <31887668+sramos02@users.noreply.github.com>
2021-12-24 17:10:54 +0100

    Update ej03.c

--- Practica2.5/ej03.c
+++ Practica2.5/ej03.c
@@ -4,10 +4,12 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#include <string.h>
+
 
 int main(int argc, char* argv[]){
 
-	if (argc != 2) {
+	if (argc != 4) {
 		perror("Invalid arguments\n");
 		return(-1);
 	}
@@ -18,43 +20,30 @@ int main(int argc, char* argv[]){
 	struct addrinfo hints;
 	struct addrinfo *result;
 	
-	memset(&hints, 0, sizeof(struct addrinfo));
+	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
 	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
 	hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
 	hints.ai_protocol = 0;          /* Any protocol */	
 
 	if(getaddrinfo(argv[1], argv[2], &hints, &result) != 0){
-		perror("Getaddrinfo error\n");
+		perror("Getaddrinfo error");
 		return -1;
 	}
 
- 	int sck = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-	
-	if(bind(sck, result->ai_addr, result->ai_addrlen) != 0){
-		perror("Bind error\n");
-		return -1;	
-	}
-	
+ 	int sck = socket(result->ai_family, result->ai_socktype, result->ai_protocol);		
+	sendto(sck, argv[3], 2, 0, result->ai_addr, result->ai_addrlen);
 	freeaddrinfo(result);
-	
-	char host[NI_MAXHOST], service[NI_MAXSERV];
-	char buf[2];
-	struct sockaddr_storage client_addr;
-	size_t client_len = sizeof(client_addr);
-	while(1){
 
-		sendto(sck, argv[3][0], 1, 0, (struct sockaddr *) &client_addr, client_len);
-	
-		s = getnameinfo((struct sockaddr *) &peer_addr, peer_addr_len, host, NI_MAXHOST,	 service, NI_MAXSERV, NI_NUMERICSERV);
+	if(*argv[3] == 'd' || *argv[3] == 't'){
+		struct sockaddr_storage client_addr;
+		socklen_t client_addrlen = sizeof(client_addr);
+		char s[50];
 
-
-
-
-		switch(argv[3]){
-			case
-		}
+		recvfrom(sck, s, 50, 0, (struct sockaddr *) &client_addr, &client_addrlen);
+		printf("%s\n", s);
 	}
+
 	
 
 }
