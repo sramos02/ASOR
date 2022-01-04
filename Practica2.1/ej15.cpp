e2af0262a1770b452c1f4ca4916d4ab8bde25634
Sergio <31887668+sramos02@users.noreply.github.com>
2022-01-04 18:57:07 +0100

    Update ej15.cpp

--- Practica2.1/ej15.cpp
+++ Practica2.1/ej15.cpp
@@ -2,62 +2,11 @@
 #include <time.h>
 using namespace std;
 
-char* dia(int numDia){
-    char* ret;
-    switch(numDia){
-	case 0: ret = "Domingo";
-	break;
-	case 1: ret = "Lunes";
-	break;
-	case 2: ret = "Martes";
-	break;
-	case 3: ret = "Miercoles";
-	break;
-	case 4: ret = "Jueves";
-	break;
-	case 5: ret = "Viernes";
-	break;
-	case 6: ret = "Sabado";
-    }
-    return ret;
-}
-
-char* mes(int numMes){
-    char* ret;
-    switch(numMes){
-	case 0: ret = "Enero";
-	break;
-	case 1: ret = "Febrero";
-	break;
-	case 2: ret = "Marzo";
-	break;
-	case 3: ret = "Abril";
-	break;
-	case 4: ret = "Mayo";
-	break;
-	case 5: ret = "Junio";
-	break;
-	case 6: ret = "Julio";
-	break;
-	case 7: ret = "Agosto";
-	break;
-	case 8: ret = "Septiembre";
-	break;
-	case 9: ret = "Octubre";
-	break;
-	case 10: ret = "Noviembre";
-	break;
-	case 11: ret = "Diciembre";
-    }
-    return ret;
-}
-
-
-
 int main(){
-    const time_t t = time(0);
-    struct tm * act = localtime(&t);
-
-    cout << dia(act->tm_wday) << ", " << act->tm_mday << " de " << mes(act->tm_mon) << " de " << act->tm_year + 1900 << ", " << act->tm_hour << ":" << act->tm_min << "\n";
+    struct tm * act = localtime(time(0));
+    char* t;
+	
+    strftime(t, 255, "%H:%M a %A, %d de %B");
+    cout << t << "de " << act->tm_year + 1900;
     return 0;
 }
