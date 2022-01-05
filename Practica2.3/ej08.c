7a7ba3c8edb4a3fd2440d2a3fcfad7fcdbe6625a
Sergio <31887668+sramos02@users.noreply.github.com>
2022-01-05 10:41:08 +0100

    Update ej08.c

--- Practica2.3/ej08.c
+++ Practica2.3/ej08.c
@@ -1,10 +1,13 @@
-#define _GNU_SOURCE
-
 #include <stdio.h>
+
 #include <stdlib.h>
-#include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
+
+#include <sys/stat.h>
 #include <fcntl.h>
+
+#include <unistd.h>
 
 int main(int argc, char* argv[]){
 	if(argc < 2){
@@ -12,36 +15,34 @@ int main(int argc, char* argv[]){
 		return -1;
 	}
 
-	int ret;
-
-	char* buf = get_current_dir_name();
-	strcat(buf, "/tmp/daemon.out");
-	printf("%s\n",buf);
-        int fd = open(buf, O_CREAT);
-        dup2(fd,1); //Copia la salida del demonio a la salida std
-        close(fd);
-
-
 	pid_t pid = fork();
-	int status; //Para el hijo
-	pid_t hijo;
-
 	switch(pid){
-		case -1:
+		case -1:;
 			perror("Fork");
-			ret = -1;
+			exit(1);
 			break;
-		case 0:
-			//HIJO
-			execvp(argv[1], argv+1);
-			printf("El comando terminó de ejecutarse\n");
-			ret = 1;
+		case 0:; //HIJO
+      		if(setsid() == -1){ //Creamos una nueva sesión
+				perror("Sid change");
+				return -1;
+			}
+			int fd = open("/tmp/daemon.out", O_CREAT | O_RDWR, 0777);
+			int fderr = open("/tmp/daemon.err", O_CREAT | O_RDWR, 0777);
+			
+			dup2(fd, 2); //Salida std
+			dup2(fderr, 1); //Salida error
+			
+			printf("Ejecutando comando...\n");
+			if (execvp(argv[1], argv+1) == -1){
+				perror("Execvp");
+				return -1;
+			}
 			break;
-		default:
-			hijo = waitpid(0, &status, 0);
-			ret = 0;
+		default:;
+			
+	        printf("El proceso se ha ejecutado correctamente\n");
 			break;
 	}
 
-	return ret;
+	return 0;
 }
